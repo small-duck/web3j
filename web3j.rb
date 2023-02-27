416e4f32c4236532e3ec684d923047995d793ca9
small-duck <1059181319@qq.com>
2023-02-27 14:38:08 +0800

    Update web3j.rb

--- web3j.rb
+++ web3j.rb
@@ -1,7 +1,7 @@
 class Web3j < Formula
   desc "web3j command line tools for Ethereum"
   homepage "https://github.com/web3j/web3j"
-  url "https://github.com/web3j/web3j-cli/releases/download/v4.8.5/web3j-4.8.5.zip"
+  url "https://github.com/web3j/web3j-cli/releases/download/v1.4.2/web3j-1.4.2.zip"
   # update with: shasum -a 256
   sha256 "8b635d0b0c1d0623b0a42711418dbd6601b53221143f84d6eb264c2713ffd5d7"
 
